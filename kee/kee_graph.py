775a5f6d7bd1ca2327351447ada5de150f6ff77f
chibighost <ureachkeerthana@gmail.com>
2026-02-09 15:31:15 +0100

    made text bigger and arrows shifted to the left to touch nodes

--- kee/kee_graph.py
+++ kee/kee_graph.py
@@ -1,8 +1,8 @@
 from manim import *
 import numpy as np
 
-config.pixel_height = 2000
-config.pixel_width = 3371
+config.pixel_height = 1080
+config.pixel_width = 1920
 config.frame_height = config.pixel_height / config.pixel_width * config.frame_width
 
 class Graph(Scene):
@@ -12,7 +12,7 @@ class Graph(Scene):
         shadow = Circle(radius = radius-0.01, color = text_color, fill_opacity = 1).move_to(position + DOWN * 0.015 + RIGHT * 0.015)
         
         border = Circle(radius = radius+0.02, color = "#dce2e1", fill_opacity = 0).move_to(position)
-        label = MathTex(label, color = text_color, stroke_color = text_color, stroke_width = 1, font_size = 70).move_to(position)
+        label = MathTex(label, color = text_color, stroke_color = text_color, stroke_width = 1.3, font_size = 100).move_to(position)
 
         label.scale(radius)
 
@@ -27,13 +27,13 @@ class Graph(Scene):
         start, end can be numpy arrays showing [x,y,z] or any mobject whose position will be taken by Arrow().
         In this case, it can be node objects from make_node()
         """
-
-        arrow = Arrow(start = start, end = end, color = color, tip_shape = tip_shape, stroke_width = 2.5, buff = 0, tip_length = 0.04)
+        
+        arrow = Arrow(start = start, end = end, color = color, tip_shape = tip_shape, stroke_width = 2.5, buff = 0, tip_length = 0.04).shift(LEFT * 0.04)
 
         angle = arrow.get_angle()
 
         #label_arrow = Text(label, weight = BOLD, color = color).next_to(arrow.get_center())
-        label_arrow = MathTex(label, color = color, stroke_color = color, stroke_width = 1, font_size = 70).move_to(arrow.point_from_proportion(0.5)).rotate(angle).shift(UP * label_shift)
+        label_arrow = MathTex(label, color = color, stroke_color = color, stroke_width = 1.3, font_size = 80).move_to(arrow.point_from_proportion(0.5)).rotate(angle).shift(UP * label_shift)
 
         label_arrow.scale(0.3)
 
@@ -48,7 +48,7 @@ class Graph(Scene):
         start = node.get_right() + DOWN * 0.15 
         end = node.get_right() + UP * 0.15
         carrow = CurvedArrow(start_point = start , end_point = end, radius=0.15, color = color, tip_shape = StealthTip, stroke_width = 3, tip_length = 0.07)
-        label_arrow = MathTex(label, color = color, stroke_color = color, stroke_width = 1, font_size = 70).move_to(carrow.point_from_proportion(0.5)).shift( RIGHT * 0.2)
+        label_arrow = MathTex(label, color = color, stroke_color = color, stroke_width = 1.3, font_size = 80).move_to(carrow.point_from_proportion(0.5)).shift( RIGHT * 0.3)
 
         label_arrow.scale(0.3)
 
