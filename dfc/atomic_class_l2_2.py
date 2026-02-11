7db89b0499ccc9e2f0aa6e67a1c0e87433ec6aac
chibighost <ureachkeerthana@gmail.com>
2026-02-11 19:21:57 +0100

    image for atomic classifier length 2

---
+++ dfc/atomic_class_l2_2.py
@@ -0,0 +1,89 @@
+"""
+TIME : 18:00 - 19:15
+
+Robust Finite Policies are Nontrivially Structured
+post link: https://www.lesswrong.com/posts/ieX8nK2b2i4JDRH5s/robust-finite-policies-are-nontrivially-structured
+
+Images for Section in Atomic Classifier
+Fig Caption: A DFC where every input has a unique input alphabet character
+
+Generate image by simple running script:
+python ./atomic_class_l1.py
+"""
+
+from manim import *
+import numpy as np
+from nodes_and_arrows import make_node, make_arrow, make_curved_arrow_right_to_left, make_curved_arrow_top, make_curved_arrow_left_to_right, make_curved_arrow_right
+
+
+config.pixel_height = 1080
+config.pixel_width = 1920
+config.frame_height = config.pixel_height / config.pixel_width * config.frame_width
+
+
+class Graph(Scene):
+    def construct(self):
+
+        # hex code of colours
+        self.camera.background_color = "#dce2e1" 
+        text_black = "#22323b"
+        node_yellow = "#e1c180"
+        node_green = "#a9c199"
+        node_blue = "#9acecc"
+        node_orange = "#d49870"
+        node_red = "#d67f86"
+
+
+        # background rectangle with rounded corners
+        background_rectangle = RoundedRectangle(color = "#d6e2e2", fill_opacity = 1, corner_radius = 0.7, 
+                                                height = 8, width = 8)
+
+    # Nodes 
+        d_left= make_node(position=[-3, 0, 0], label="d", node_color = node_blue)
+
+        d_top = make_node(position=[-0.75,2.25,0], label="d") 
+
+        d_bot = make_node(position=[-0.75,-2.25,0], label="d")
+
+        a2_top = make_node(position=[1.75,2.25,0], label="a_2", node_color=node_green) 
+
+        a1_bot = make_node(position=[1.75,-2.25,0], label="a_1", node_color=node_orange) 
+
+        d_right = make_node(position=[4,0,0], label="d") 
+         
+
+    # Arrows
+        arrow_1 = make_arrow(start = d_left, end = d_top, label = "0", scale_label = 0.4, label_shift = 0.3)
+
+        arrow_2 = make_arrow(start = d_left, end = d_bot, label = "1", scale_label = 0.4, label_shift = 0.3)
+        
+        arrow_3 = make_arrow(start = d_top, end = a2_top, label = "1", scale_label = 0.4)
+        
+        arrow_4 = make_arrow(start = d_top, end = a1_bot, label = "0", scale_label = 0.4, label_shift = 0.4)
+
+        arrow_5 = make_arrow(start = d_bot, end = a1_bot, label = "0,1", scale_label = 0.4)
+        
+        arrow_6 = make_arrow(start = a2_top, end = d_right, label = "0,1", scale_label = 0.4)
+
+        arrow_7 = make_arrow(start = a1_bot, end = d_right, label = "0,1", scale_label = 0.4)
+        
+        arrow_8 = make_curved_arrow_right(start_node = d_right, end_node = d_right, color=text_black, label= "0,1", radius = 0.15, label_scale = 0.4)
+
+    # Triangle 
+        triangle = Triangle(color = text_black, fill_opacity=1).rotate(270*DEGREES)
+        triangle.scale(0.3).shift(LEFT * 3.77 + DOWN * 0.25)
+   
+        all_nodes = VGroup(d_left, d_right, d_bot, d_top, a1_bot, a2_top)
+       
+        all_arrows = VGroup(arrow_1, arrow_2, arrow_3, arrow_4, arrow_5, arrow_6, arrow_7, arrow_8)
+
+        all_objects = VGroup(all_arrows, all_nodes, triangle)
+        self.add(all_objects)
+        
+        #debug distance of objects with a grid
+        #self.add(NumberPlane())
+
+        
+with tempconfig({"preview": False}):
+    scene = Graph()
+    scene.render()
--- dfc/nodes_and_arrows.py
+++ dfc/nodes_and_arrows.py
@@ -17,26 +17,26 @@ def make_node(position, label="x", radius=0.5, node_color="#e1c180", text_color=
 
 #def make_arrow(self, start, end, label = "x", buff=0.5, color = "#22323b", tip_shape = StealthTip, stroke_width = 7, tip_length = 0.25):
 
-def make_arrow(start, end, label="x", label_shift=0.2, color="#22323b", tip_shape = StealthTip):
+def make_arrow(start, end, label="x", label_shift=0.2, color="#22323b", tip_shape = StealthTip,  scale_label = 0.3, tip_length = 0.07):
     """
     start, end can be numpy arrays showing [x,y,z] or any mobject whose position will be taken by Arrow().
     In this case, it can be node objects from make_node()
     """
     
-    arrow = Arrow(start = start, end = end, color = color, tip_shape = tip_shape, stroke_width = 2.5, buff = 0, tip_length = 0.04).shift(LEFT * 0.04)
+    arrow = Arrow(start = start, end = end, color = color, tip_shape = tip_shape, stroke_width = 2.5, buff = 0, tip_length = tip_length).shift(LEFT * 0.04)
 
     angle = arrow.get_angle()
 
     #label_arrow = Text(label, weight = BOLD, color = color).next_to(arrow.get_center())
     label_arrow = MathTex(label, color = color, stroke_color = color, stroke_width = 1.3, font_size = 80).move_to(arrow.point_from_proportion(0.5)).rotate(angle).shift(UP * label_shift)
 
-    label_arrow.scale(0.3)
+    label_arrow.scale(scale_label)
 
     arrow_with_label = VGroup(arrow, label_arrow)
 
     return arrow_with_label
 
-def make_curved_arrow_top(start_node, end_node, color, label, radius):
+def make_curved_arrow_top(start_node, end_node, color, label, radius, label_scale = 0.3):
     """
     node is a vgroup object returned by make_node()
     """
@@ -45,11 +45,26 @@ def make_curved_arrow_top(start_node, end_node, color, label, radius):
     carrow = CurvedArrow(start_point = start , end_point = end, radius=radius, color = color, tip_shape = StealthTip, stroke_width = 3, tip_length = 0.07)
     label_arrow = MathTex(label, color = color, stroke_color = color, stroke_width = 1.3, font_size = 80).move_to(carrow.point_from_proportion(0.5)).shift(UP * 0.2)
 
-    label_arrow.scale(0.3)
+    label_arrow.scale(label_scale)
 
     carrow_with_label = VGroup(carrow, label_arrow)
 
     return carrow_with_label
+
+def make_curved_arrow_right(start_node, end_node, color, label, radius, label_scale = 0.3):
+        """
+        node is a vgroup object returned by make_node()
+        """
+        start = start_node.get_right() + DOWN * 0.15 
+        end = end_node.get_right() + UP * 0.15
+        carrow = CurvedArrow(start_point = start , end_point = end, radius=radius, color = color, tip_shape = StealthTip, stroke_width = 3, tip_length = 0.07)
+        label_arrow = MathTex(label, color = color, stroke_color = color, stroke_width = 1.3, font_size = 80).move_to(carrow.point_from_proportion(0.5)).shift( RIGHT * 0.3)
+
+        label_arrow.scale(label_scale)
+
+        carrow_with_label = VGroup(carrow, label_arrow)
+
+        return carrow_with_label
 
 def make_curved_arrow_right_to_left(start_node, end_node, color, label, radius):
     """
