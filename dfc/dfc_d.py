0e3a5a2171b4e8caf6baed82bd38a2cb9dceb9b4
chibighost <ureachkeerthana@gmail.com>
2026-02-11 16:51:17 +0100

    dfc with d and added triangles to the previous images

--- dfc/dfc_d.py
+++ dfc/dfc_d.py
@@ -1,14 +1,14 @@
 """
-TIME : 14:35 - 16:10
+TIME : 16:15 - 16:50
 
 Robust Finite Policies are Nontrivially Structured
 post link: https://www.lesswrong.com/posts/ieX8nK2b2i4JDRH5s/robust-finite-policies-are-nontrivially-structured
 
-Images for Section in Definitions
-Fig Caption: A DFC for a policy which sends inputs that end in 0 to action a1 and sends inputs ending in 1 to action a2
+Images for Section in Special Policies
+Fig Caption: A DFC for a default policy which always outputs the action d
 
 Generate image by simple running script:
-python ./dfc_a1a2.py
+python ./dfc_d.py
 """
 
 from manim import *
@@ -32,6 +32,7 @@ class Graph(Scene):
         node_blue = "#9acecc"
         node_orange = "#d49870"
         node_red = "#d67f86"
+        background_color = "#dce2e1" 
 
 
         # background rectangle with rounded corners
@@ -39,33 +40,31 @@ class Graph(Scene):
                                                 height = 8, width = 8)
 
     # Nodes 
-        a1 = make_node(position=[-2,0,0], radius=0.4, label="a_1", node_color=node_orange) 
-
-        a2 = make_node(position=[2,0,0], radius=0.4, label="a_2", node_color=node_green) 
+        d = make_node(position=[0,0,0], radius=0.4, label="d") 
 
     # Arrows
-        arrow_1 = make_curved_arrow_left_to_right(start_node = a1, end_node = a2, color=text_black, label= "1", radius = 6).shift(DOWN * 0.05 + LEFT * 0.04)
-        
-        arrow_2 = make_curved_arrow_right_to_left(start_node = a2, end_node = a1, color=text_black, label= "0", radius = 6).shift(UP * 0.05 + RIGHT * 0.04)
-
-        arrow_3 = make_curved_arrow_top(start_node = a1, end_node = a1, color=text_black, label= "0", radius = 0.21)
-
-        arrow_4 = make_curved_arrow_top(start_node = a2, end_node = a2, color=text_black, label= "1", radius = 0.21)
+        arrow_1 = make_curved_arrow_top(start_node = d, end_node = d, color=text_black, label= "0,1", radius = 0.21)
 
     # Triangle 
         triangle = Triangle(color = text_black, fill_opacity=1).rotate(270*DEGREES)
-        triangle.scale(0.3).shift(LEFT * 2.67 + DOWN * 0.25)
-   
-        all_nodes = VGroup(a1, a2)
-       
-        all_arrows = VGroup(arrow_1, arrow_2, arrow_3, arrow_4)
+        triangle.scale(0.3).shift(LEFT * 0.68 + DOWN * 0.25)
 
+        #triangle_tip = Triangle(color = background_color, fill_opacity=1).rotate(270*DEGREES)
+        #triangle_tip.scale(0.06).shift(LEFT * 0.6 + DOWN * 0.25)
+   
+        all_nodes = VGroup(d)
+       
+        all_arrows = VGroup(arrow_1)
+
+        #all_objects = VGroup(all_arrows, all_nodes, triangle, triangle_tip).center()
         all_objects = VGroup(all_arrows, all_nodes, triangle).center()
 
         self.add(all_objects)
         
         #debug distance of objects with a grid
         self.add(NumberPlane())
+    
+
 
         
 with tempconfig({"preview": False}):
