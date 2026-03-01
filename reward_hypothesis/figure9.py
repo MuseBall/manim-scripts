50fc758ee31d508ccdfaba9760aa10ba511b9360
chibighost <ureachkeerthana@gmail.com>
2026-03-01 13:46:46 +0100

    moved (0,0) away from the origin in fig 8 and 9

--- reward_hypothesis/figure9.py
+++ reward_hypothesis/figure9.py
@@ -1,14 +1,13 @@
 """
-TIME : 16:50 - 17:20
-        15:40 - 17:00
+TIME : 17:10 - 18:15
 
 Guille - Reward Hypothesis - Dovetail Project
 
 Images for Section in Set - Order representation - Preorder and Debreu separable - Debreu-Fishburns theorem
-Figure 8: Lexicographical linearization: infinity. 
+Figure 9: Lexicographical linearization: asymptotic
 
 Generate image by simple running script:
-python ./figure8.py
+python ./figure9.py
 """
 
 from manim import *
@@ -42,39 +41,43 @@ class Graph(Scene):
 
         axes = Axes(x_range =[-1.5,3.5], y_range = [-3,3], x_length= 8, y_length = 7, x_axis_config={"include_numbers": False, "include_ticks": False}, y_axis_config={"include_numbers": False, "include_ticks": False}, axis_config = {"tip_shape": StealthTip, "color": text_black}).move_to([4,0,0])
 
-        labels_axes = axes.get_axis_labels(MathTex("w_a", color = text_black, stroke_width = 1.5).scale(0.8), MathTex("w_p", color = text_black, stroke_width = 1.5).scale(0.8))
+        labels_axes = axes.get_axis_labels(MathTex("", color = text_black, stroke_width = 1.5).scale(0.8), MathTex("1", color = text_black, stroke_width = 1.5).scale(0.8))
 
-        labels_axes[0].next_to(axes.get_x_axis().get_right(), RIGHT)
         labels_axes[1].next_to(axes.get_y_axis().get_top(), UP)
 
     # Points in graph
 
         point_0 = axes.coords_to_point(0, 0)
-        point_1 = axes.coords_to_point(0, 1)
-        point_k = axes.coords_to_point(0, 1.7)
-        point_inf = axes.coords_to_point(1, 0)
+        point_1e = axes.coords_to_point(0, 0.7)
+        point_1ek = axes.coords_to_point(0, 1.4)
+        point_1 = axes.coords_to_point(0.7, 0)
+        point_2 = axes.coords_to_point(1.4, 0)
 
         dot_0 = Dot(point_0, color = node_purple, stroke_color = text_black, stroke_width = 1)
+        dot_1e = Dot(point_1e, color = node_purple, stroke_color = text_black, stroke_width = 1)
+        dot_1ek = Dot(point_1ek, color = node_purple, stroke_color = text_black, stroke_width = 1)
         dot_1 = Dot(point_1, color = node_purple, stroke_color = text_black, stroke_width = 1)
-        dot_k = Dot(point_k, color = node_purple, stroke_color = text_black, stroke_width = 1)
-        dot_inf = Dot(point_inf, color = node_purple, stroke_color = text_black, stroke_width = 1)
+        dot_2 = Dot(point_2, color = node_purple, stroke_color = text_black, stroke_width = 1)
 
-        label_0 = MathTex("0", font_size = 60).scale(0.6).set_color(text_black).next_to(dot_0, LEFT).shift(UP * 0.2)
-        label_1 = MathTex("1", font_size = 60).scale(0.6).set_color(text_black).next_to(dot_1, LEFT)
-        label_k = MathTex("K", font_size = 60).scale(0.6).set_color(text_black).next_to(dot_k, LEFT)
-        label_inf = MathTex("?> \infty", font_size = 60).scale(0.6).set_color(text_black).next_to(dot_inf, UP)
+        label_0 = MathTex("0", font_size = 60).scale(0.6).set_color(text_black).next_to(dot_0, LEFT).shift(DOWN * 0.2)
+        label_1e = MathTex(r"1 - \frac{1}{e}", font_size = 60).scale(0.6).set_color(text_black).next_to(dot_1e, LEFT)
+        label_1ek = MathTex(r"1-e^{-k}", font_size = 60).scale(0.6).set_color(text_black).next_to(dot_1ek, LEFT)
+        label_1 = MathTex("1", font_size = 60).scale(0.6).set_color(text_black).next_to(dot_1, UP)
+        label_2 = MathTex("2>1", font_size = 60).scale(0.6).set_color(text_black).next_to(dot_2, UP)
 
-        ellipsis = MathTex(rf"\vdots", font_size = 60).scale(0.6).set_color(text_black).next_to(label_k, UP).shift(UP * 0.1)
+        bracket = MathTex(r"\{", font_size = 65).scale(1).set_color(text_black).next_to(dot_0, UP).rotate(-PI/2).shift(LEFT * 3.25 + DOWN * 0.13)
 
-        infinity = MathTex(rf"\infty", font_size = 60).scale(0.6).set_color(text_black)
-        infinity.next_to(axes.get_y_axis().get_top(), LEFT).shift(DOWN * 0.3 + LEFT *0.1)
+        epsilon = MathTex(r"\epsilon", font_size = 60).scale(0.6).set_color(text_black).next_to(bracket, UP).shift(DOWN * 0.13)
+
+        ellipsis = MathTex(rf"\vdots", font_size = 60).scale(0.6).set_color(text_black).next_to(label_1ek, UP).shift(UP * 0.1)
+
 
     # Sub-Graph
 
         sub_axes = Axes(
             x_range=[-0.3, 4],
             y_range=[-0.3, 4.5],
-            x_length=4.25,
+            x_length=4,
             y_length=2.5,
             x_axis_config={"include_numbers": False, "include_ticks": False},
             y_axis_config={"include_numbers": False, "include_ticks": False},
@@ -95,28 +98,37 @@ class Graph(Scene):
         point_0k = sub_axes.coords_to_point(1.5, 0)
         point_0e0 = sub_axes.coords_to_point(2.5, 0)
         point_10 = sub_axes.coords_to_point(3.5, 0)
+        point_sub1 = sub_axes.coords_to_point(0,1.5)
+        point_sub2 = sub_axes.coords_to_point(0,3)
 
         dot_00 = Dot(point_00, color = node_green, stroke_color = text_black, stroke_width = 1)
         dot_0k = Dot(point_0k, color = node_green, stroke_color = text_black, stroke_width = 1)
         dot_0e0 = Dot(point_0e0, color = node_green, stroke_color = text_black, stroke_width = 1)
         dot_10 = Dot(point_10, color = node_green, stroke_color = text_black, stroke_width = 1)
+        dot_sub1 = Dot(point_sub1, color = node_green, stroke_color = text_black, stroke_width = 1)
+        dot_sub2 = Dot(point_sub2, color = node_green, stroke_color = text_black, stroke_width = 1)
 
         label_00 = MathTex("(0,0)", font_size = 50).scale(0.6).set_color(text_black).next_to(dot_00, DOWN)
         label_0k = MathTex("(0,k)", font_size = 50).scale(0.6).set_color(text_black).next_to(dot_0k, DOWN)
         label_0e0 = MathTex("(0+\epsilon, 0)", font_size = 50).scale(0.6).set_color(text_black).next_to(dot_0e0, DOWN)
         label_10 = MathTex("(1,0)", font_size = 50).scale(0.6).set_color(text_black).next_to(dot_10, DOWN)
+        label_sub1 = MathTex("1", font_size = 50).scale(0.6).set_color(text_black).next_to(dot_sub1, LEFT)
+        label_sub2 = MathTex("2", font_size = 50).scale(0.6).set_color(text_black).next_to(dot_sub2, LEFT)
 
     # Curve in sub-graph
-        arc = ArcBetweenPoints(start = point_00 , end = point_0e0 + UP * 1.5 + LEFT * 0.1, radius= 1.7, color = text_black, stroke_width = 3).shift(UP * 0.05)
+        arc = ArcBetweenPoints(start = point_00 , end = point_0e0 + UP * 0.7, radius= -2.5, color = text_black, stroke_width = 3).shift(UP * 0.05)
 
     # Lines in Sub-graph
-        line_0k = DashedLine(point_0k + UP * 0.22, point_0k, dash_length=0.15, color=text_black, stroke_width=3)
+        line_0k = DashedLine(point_0k + UP * 0.6, point_0k, dash_length=0.15, color=text_black, stroke_width=3)
         line_0e0 = DashedLine(point_0e0 + UP * 1.8, point_0e0, dash_length=0.15, color=text_black, stroke_width=3)
         line_10 = DashedLine(point_10 + UP * 1.8, point_10, dash_length=0.15, color=text_black, stroke_width=3)
 
-        label_line_10 = MathTex("?", font_size = 50).scale(0.6).set_color(text_black).next_to(line_10, UP)
+        line_sub1 = DashedLine(point_sub1, point_sub1 + RIGHT * 3, dash_length=0.15, color=text_black, stroke_width=3)
+        line_sub2 = DashedLine(point_sub2, point_sub2 + RIGHT * 3, dash_length=0.15, color=text_black, stroke_width=3)
 
-        self.add(arc, line_0k, line_0e0, line_10, sub_axes, sub_labels, dot_00, label_00, dot_0k, label_0k, dot_0e0, label_0e0, dot_10, label_10, label_line_10)
+        label_line_10 = MathTex("?", font_size = 50).scale(0.6).set_color(text_black).next_to(line_10, UP).set_opacity(0)
+
+        self.add(arc, line_0k, line_0e0, line_10, line_sub1, line_sub2, sub_axes, sub_labels, dot_00, label_00, dot_0k, label_0k, dot_0e0, label_0e0, dot_10, label_10, label_line_10, dot_sub1, dot_sub2, label_sub1, label_sub2, bracket, epsilon)
 
 
     #Lines in graph
@@ -127,7 +139,7 @@ class Graph(Scene):
         label = MathTex("x \succeq y \succeq z", font_size = 70, stroke_width = 2).set_color(text_black).next_to(axes, DOWN * 2).set_opacity(0)
 
 
-        all_objects = VGroup(axes, dot_0, dot_1, dot_k, dot_inf, label_inf, labels_axes, label_0, label_1, label_k, label, ellipsis, infinity).center().scale(0.8)
+        all_objects = VGroup(axes, dot_0, dot_1e, dot_1ek, dot_1, dot_2, labels_axes, label_0, label_1e, label_1ek, label_1, label_2, label, ellipsis).center().scale(0.8)
         self.add(all_objects)
         
         #debug distance of objects with a grid
