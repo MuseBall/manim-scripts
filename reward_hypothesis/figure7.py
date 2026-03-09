3964b31ff7b2b9074c224693f50ee9ebbb55ded8
chibighost <ureachkeerthana@gmail.com>
2026-03-10 00:21:56 +0100

    changed dashed line color

--- reward_hypothesis/figure7.py
+++ reward_hypothesis/figure7.py
@@ -63,8 +63,8 @@ class Graph(Scene):
 
     #Lines in graph
 
-        line_AB = DashedLine(point_A + UP * 2, point_B + DOWN * 5, dash_length=0.15, color=text_black, stroke_width=3)
-        line_C = DashedLine(point_C + UP * 4.3, point_C + DOWN * 1.8, dash_length=0.15, color=text_black, stroke_width=3)
+        line_AB = DashedLine(point_A + UP * 2, point_B + DOWN * 5, dash_length=0.15, color="#739b99", stroke_width=3)
+        line_C = DashedLine(point_C + UP * 4.3, point_C + DOWN * 1.8, dash_length=0.15, color="#739b99", stroke_width=3)
 
         label = MathTex("x \succsim y \succsim z", font_size = 70, stroke_width = 2).set_color(text_black).next_to(axes, DOWN)
 
