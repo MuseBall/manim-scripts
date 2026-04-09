df3617900e0945b32da90fddcba71972d72bd70d
chibighost <ureachkeerthana@gmail.com>
2026-04-09 23:03:47 +0200

    figure 23 grid problems

--- reward_hypothesis/figure23.py
+++ reward_hypothesis/figure23.py
@@ -13,6 +13,13 @@ python ./figure22.py
 from manim import *
 import numpy as np
 from nodes_and_arrows import make_arrow
+from plttriangle import (
+    function1D_graph,
+    function1D_heatmap,
+    function2D_graph,
+    function2D_heatmap,
+)
+
 
 
 config.pixel_height = 1080
@@ -20,7 +27,7 @@ config.pixel_width = 1920
 config.frame_height = config.pixel_height / config.pixel_width * config.frame_width
 
 
-class Graph(Scene):
+class Graph(ThreeDScene):
     def construct(self):
 
         # hex code of colours
@@ -38,152 +45,71 @@ class Graph(Scene):
         # background rectangle with rounded corners
         background_rectangle = RoundedRectangle(color = "#d6e2e2", fill_opacity = 1, corner_radius = 0.7, height = 8, width = 8)
 
-    # Triangle 1  
+        ##### TOP LEFT GRAPH 22R
 
-        triangle1 = Polygon(
-            [0,0.8,0],
-            [-1.5, -1,0],
-            [1.5, -1, 0],
-            stroke_color = text_black, 
-            stroke_width = 2,
-            color = node_blue,
-            fill_opacity = 1
-            ).set_sheen(0.5, LEFT)
+        # Bottom right graph 1 (NOT GAMMA INDIFFERENT)
+        function2D_graph_1 = function2D_graph(lambda x, y: x / 2 + 2.5, points=True, x_label="", y_label="", z_label = "")
 
-        triangle1top = Polygon(
-            [0,1.5,0],
-            [-1.5, -0.5,0],
-            [1.5, 1, 0],
-            stroke_color = text_black, 
-            stroke_width = 2
+        function2D_graph_1.shift(LEFT * 4.4).scale(0.65)
+
+        # Bottom right graph 2 (NOT GAMMA INDIFFERENT)
+        function2D_graph_2 = function2D_graph(lambda x, y: x / 2 + 1.5, points=True, x_label="", y_label="", z_label = "")
+
+        function2D_graph_2.shift(LEFT * 1.6).scale(0.65)
+
+        left_top_graphs = Group(
+            function2D_graph_1,
+            function2D_graph_2,
             )
 
-    # Triangle 2  
+        left_top = self.add(left_top_graphs).set_camera_orientation(phi=60 * DEGREES, theta=-90 * DEGREES)
 
-        triangle2 = Polygon(
-            [0,0.8,0],
-            [-1.5, -1,0],
-            [1.5, -1, 0],
-            stroke_color = text_black, 
-            stroke_width = 2,
-            color = node_blue,
-            fill_opacity = 1
-            ).set_sheen(0.5, RIGHT)
 
-        triangle2top = Polygon(
-            [0,1.5,0],
-            [-1.5, -0.5,0],
-            [1.5, 0.6, 0],
-            stroke_color = text_black, 
-            stroke_width = 2
+        ##### TOP RIGHT GRAPH 22L
+
+         # Bottom left graph 1 (GAMMA INDIFFERENT SCALED)
+        function2D_graph_3 = function2D_graph(lambda x, y: x / 2 + 2.5, points=True, x_label="", y_label="", z_label = "")
+
+        function2D_graph_3.shift(RIGHT * 1.6).scale(0.65)
+
+        # Bottom left graph 2 (GAMMA INDIFFERENT SCALED)
+        function2D_graph_4 = function2D_graph(lambda x, y: (x / 2 + 2.5)*0.3, points=True, x_label="", y_label="", z_label = "")
+
+        function2D_graph_4.shift(RIGHT * 4.4).scale(0.65)
+
+        right_top_graphs = Group(
+            function2D_graph_3,
+            function2D_graph_4,
             )
 
-    # Triangle 3  
+        right_top = self.add(right_top_graphs).set_camera_orientation(phi=60 * DEGREES, theta=-90 * DEGREES)
 
-        triangle3 = Polygon(
-            [0,0.8,0],
-            [-1.5, -1,0],
-            [1.5, -1, 0],
-            stroke_color = text_black, 
-            stroke_width = 2,
-            color = node_blue,
-            fill_opacity = 1
-            ).set_sheen(0.5, LEFT)
+        ##### BOTTOM RIGHT GRAPH 21L
 
-        triangle3top = Polygon(
-            [0,1.5,0],
-            [-1.5, -0.5,0],
-            [1.7, 0.6, 0],
-            stroke_color = text_black, 
-            stroke_width = 2
+        # Bottom left graph 1 (Not Dynamically Consistent)
+        function2D_graph_5 = function2D_graph(lambda x, y: x / 2 + 2.5, points=True, x_label="", y_label="", z_label = "")
+
+        function2D_graph_5.shift(RIGHT * 1.8).scale(0.65)
+
+        # Bottom left graph 2 (Not Dynamically Consistent)
+        function2D_graph_6 = function2D_graph(lambda x, y: -x / 3 + 2.5, points=True, x_label="", y_label="", z_label = "")
+
+        function2D_graph_6.shift(RIGHT * 0.9).scale(0.65)
+
+        right_bot_graph = Group(
+            function2D_graph_5,
+            function2D_graph_6,
             )
 
-    # Triangle 4  
+        right_bot = self.add(right_bot_graph).set_camera_orientation(phi=60 * DEGREES, theta=-90 * DEGREES)
 
-        triangle4 = Polygon(
-            [0,0.8,0],
-            [-1.5, -1,0],
-            [1.5, -1, 0],
-            stroke_color = text_black, 
-            stroke_width = 2,
-            color = node_blue,
-            fill_opacity = 1
-            ).set_sheen(0.5, LEFT)
-
-        triangle4top = Polygon(
-            [0,1.2,0],
-            [-1.5, -0.6,0],
-            [1.7, 0.9, 0],
-            stroke_color = text_black, 
-            stroke_width = 2
-            )
-
-    # Triangle 5  
-
-        triangle5 = Polygon(
-            [0,0.8,0],
-            [-1.5, -1,0],
-            [1.5, -1, 0],
-            stroke_color = text_black, 
-            stroke_width = 2,
-            color = node_blue,
-            fill_opacity = 1
-            ).set_sheen(0.5, LEFT)
-
-        triangle5top = Polygon(
-            [0,1.5,0],
-            [-1.5, -0.5,0],
-            [1.5, 1, 0],
-            stroke_color = text_black, 
-            stroke_width = 2
-            )
-
-    # Triangle 6  
-
-        triangle6 = Polygon(
-            [0,0.8,0],
-            [-1.5, -1,0],
-            [1.5, -1, 0],
-            stroke_color = text_black, 
-            stroke_width = 2,
-            color = node_blue,
-            fill_opacity = 1
-            ).set_sheen(0.5, RIGHT)
-
-        triangle6top = Polygon(
-            [0,1.5,0],
-            [-1.5, 0.8,0],
-            [1.5, -0.5, 0],
-            stroke_color = text_black, 
-            stroke_width = 2
-            )
+        ##### BOTTOM LEFT TEXT
 
         text1 = MathTex(r" \gamma - \text{Indifferent}", color = text_black).shift(UP * 0.7)
         text2 = MathTex(r"\Downarrow", color = text_black)
         text3 = MathTex(r"\text{Dynamically Consistent}", color = text_black).shift(DOWN * 0.7)
 
-        text = VGroup(text1, text2, text3)
-
-
-        triangle_left_bot = VGroup(triangle1, triangle1top).to_edge(LEFT).shift(UP * 1)
-
-        triangle_left_bot2 = VGroup(triangle2, triangle2top).to_edge(LEFT).shift(RIGHT * 3.2 + UP * 1)
-
-        triangle_right_bot = VGroup(triangle3, triangle3top).to_edge(RIGHT).shift(LEFT * 3.2 + UP * 1)
-
-        triangle_right_bot2 = VGroup(triangle4, triangle4top).to_edge(RIGHT).shift(UP * 1)
-
-        triangle_right_bot21 = VGroup(triangle5, triangle5top).to_edge(RIGHT).shift(UP * 1)
-
-        triangle_right_bot22 = VGroup(triangle6, triangle6top).to_edge(RIGHT).shift(UP * 1 + LEFT * 3.2)
-
-        all_=VGroup(triangle_left_bot, triangle_left_bot2, triangle_right_bot, triangle_right_bot2).center()
-
-        left_top = VGroup(triangle_left_bot, triangle_left_bot2)
-        right_top = VGroup(triangle_right_bot, triangle_right_bot2)
-        right_bot = VGroup(triangle_right_bot21, triangle_right_bot22)
-
-
+        text = Group(text1, text2, text3)
 
     # Table
 
@@ -204,8 +130,19 @@ class Graph(Scene):
         table.get_horizontal_lines().set_color(text_black)
         table.get_vertical_lines().set_color(text_black)
 
+        all_3D = Group(
+            function2D_graph_1,
+            function2D_graph_2,
+            function2D_graph_3,
+            function2D_graph_4,
+            function2D_graph_5,
+            function2D_graph_6,
+        )
 
-        self.add(table)
+        #self.add(all_3D)
+        self.add_fixed_in_frame_mobjects(table)
+        #self.add_fixed_in_frame_mobjects(all_2D, labels, NumberPlane())
+        self.set_camera_orientation(phi=60 * DEGREES, theta=-90 * DEGREES)
         
         #debug distance of objects with a grid
         #self.add(NumberPlane())
